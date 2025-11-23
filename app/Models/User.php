0a0e755b9e858bdf8dd904082d6869eb8c208dec
PlotHooks <125180961+PlotHooks@users.noreply.github.com>
2025-11-23 04:23:11 +0000

    Add Character relationship import to User model

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,6 +6,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use App\Models\Character; // ⬅ ADD THIS LINE
 
 class User extends Authenticatable
 {
@@ -45,9 +46,9 @@ class User extends Authenticatable
             'password' => 'hashed',
         ];
     }
-public function characters()
-{
-    return $this->hasMany(Character::class);
-}
 
+    public function characters()
+    {
+        return $this->hasMany(Character::class);
+    }
 }
